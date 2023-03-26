1a7a5599aa2ea5f3fe5fe55c666ac13ea3587ee8
Dmitry Biryukov <dim.bir2017@yandex.ru>
2023-03-26 20:31:18 +0300

    Добавлена пагинация. Добавлена фильтрация публикаций по заголовку, автору и дате. Добавлены страницы добавления, изменения и удаления публикаций.

---
+++ news/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from django.forms import DateInput
+from django_filters.widgets import RangeWidget
+
+from django_filters import FilterSet, ModelMultipleChoiceFilter
+
+from .models import Post, Author
+
+
+class PostFilter(FilterSet):
+    title = django_filters.CharFilter(label='Заголовок статьи', lookup_expr='icontains')
+    author = ModelMultipleChoiceFilter(queryset=Author.objects.all(), label='Автор', conjoined=True)
+    time_in = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}), label='Статьи поздее:', lookup_expr='date__gt')
+
+    class Meta:
+        model = Post
+        fields = ['title', 'author', 'time_in']
