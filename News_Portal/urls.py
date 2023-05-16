25a2df365c8a678e40b9edc0cd833ef0f4f80789
Dmitry Biryukov <dim.bir2017@yandex.ru>
2023-05-16 11:43:35 +0300

    Добавлены Celery и переменные окружения

--- News_Portal/urls.py
+++ News_Portal/urls.py
@@ -1,4 +1,4 @@
-"""NP URL Configuration
+"""News_Portal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- News_Portal/wsgi.py
+++ News_Portal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NP project.
+WSGI config for News_Portal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'News_Portal.settings')
 
 application = get_wsgi_application()
